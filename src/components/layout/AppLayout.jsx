454eee7da72cb1b27a4ace8abd6668fb6ca3e644
itay-messika301 <itay@luca-ai.io>
2026-04-22 10:55:17 +0300

    Update AppLayout.jsx

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,12 +1,11 @@
-import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
 
-export default function AppLayout({ isClient = false }) {
+export default function AppLayout({ children, isClient = false }) {
   return (
     <div className="flex min-h-screen bg-[#F8F8FA]">
       <Sidebar isClient={isClient} />
-      <main className="flex-1 mr-60 p-8">
-        <Outlet />
+      <main className="flex-1 overflow-auto">
+        {children}
       </main>
     </div>
   )
